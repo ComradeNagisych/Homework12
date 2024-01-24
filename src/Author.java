78413242d12ff770cb275ee8762b2d75c09bb964
kiril <penzaringo2010@yandex.ru>
2024-01-24 15:37:42 +0300

    Сделано ещё ДЗ

--- src/Author.java
+++ src/Author.java
@@ -1,11 +1,28 @@
+import java.util.Objects;
+
 public class Author {
-    private String firstName;
-    private String lastName;
-    public Author (String firstName, String lastName){
+    private final String firstName;
+    private final String lastName;
+    public Author (String firstName, String lastName) {
         this.firstName = firstName;
         this.lastName = lastName;
     }
+    @Override
+    public boolean equals(Object other) {
+        if (this == other) return true;
+        if (other == null || getClass() != other.getClass()) return false;
+        Author author = (Author) other;
+        return Objects.equals(firstName, author.firstName) && Objects.equals(lastName, author.lastName);
+    }
+    @Override
+    public int hashCode () {
+        return Objects.hash(firstName, lastName);
+    }
+    @Override
     public String toString() {
-        return firstName + " " + lastName;
+        return "Author{" +
+                "firstName = " + firstName +
+                ", lastName = " + lastName +
+                "}";
     }
 }
